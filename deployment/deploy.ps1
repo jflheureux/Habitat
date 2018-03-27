43e6b0fdbf8d59a6b4f2e07854ebbf17c9dcf27b
Jean-François L'Heureux <spot_web@hotmail.com>
2018-03-27 09:12:47 -0400

    Allow HTTP calls from deployment scripts to run without IE initial setup

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -1,7 +1,7 @@
 # Installation
 Write-Host "Calling package installer..."
 $url = "$env:appveyor_deployment_url/InstallPackage.aspx"
-$result = Invoke-WebRequest -Uri $url -TimeoutSec 600 -OutFile ".\InstallPackage-Response.log" -PassThru
+$result = Invoke-WebRequest -Uri $url -TimeoutSec 600 -OutFile ".\InstallPackage-Response.log" -PassThru -UseBasicParsing
 if($result.StatusCode -ne 200) {
     Write-Host "StatusCode: $($result.StatusCode)"
     throw "Package install failed."
@@ -11,7 +11,7 @@ Write-Host "Package install successful."
 # Publishing
 Write-Host "Calling publisher..."
 $url = "$env:appveyor_deployment_url/Publish.aspx"
-$result = Invoke-WebRequest -Uri $url -TimeoutSec 600 -OutFile ".\Publish-Response.log" -PassThru
+$result = Invoke-WebRequest -Uri $url -TimeoutSec 600 -OutFile ".\Publish-Response.log" -PassThru -UseBasicParsing
 if($result.StatusCode -ne 200) {
     Write-Host "StatusCode: $($result.StatusCode)"
     throw "Publish failed."
